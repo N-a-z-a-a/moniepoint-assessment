a20bcff30916df834111f1266d25cbeef71dfe8b
N-a-z-a-a <jedwin.ezeh@gmail.com>
2023-04-30 17:02:40 +0100

    add date feature to stat page and install reacter router

--- tech-assessment/src/components/MenuTab.jsx
+++ tech-assessment/src/components/MenuTab.jsx
@@ -1,10 +1,10 @@
 function MenuTab() {
   return (
     <div className="flex flex-row justify-around items-center bg-white px-5 py-7 rounded-t-3xl w-full fixed bottom-0 menu-tab">
-      <h2 className="text-gray-500">1</h2>
-      <h2 className="text-gray-500">1</h2>
-      <h2 className="text-gray-500">1</h2>
-      <h2 className="text-gray-500">1</h2>
+      <h2 className="text-gray-500">HO</h2>
+      <h2 className="text-gray-500">ST</h2>
+      <h2 className="text-gray-500">PE</h2>
+      <h2 className="text-gray-500">SE</h2>
     </div>
   );
 }
--- tech-assessment/src/pages/StatsPage.jsx
+++ tech-assessment/src/pages/StatsPage.jsx
@@ -2,9 +2,77 @@ import NavigationTab from "../components/NavigationTab";
 import SubjectProgress from "../components/SubjectProgress";
 import MenuTab from "../components/MenuTab";
 
+function Date() {
+  const dates = [
+    {
+      date: 2,
+      day: "Mon",
+    },
+    {
+      date: "&#10003;",
+      day: "Tue",
+    },
+    {
+      date: "&#10003;",
+      day: "Wed",
+    },
+    {
+      date: 5,
+      day: "Thurs",
+    },
+    {
+      date: 6,
+      day: "Fri",
+    },
+    {
+      date: 7,
+      day: "Sat",
+    },
+    {
+      date: 8,
+      day: "Sun",
+    },
+  ];
+
+  return dates.map((date) => {
+    return (
+      <div className="flex flex-col justify-center items-center gap-2">
+        <div
+          className={
+            date.date === 5
+              ? "flex justify-center items-center h-5 w-5 rounded-full text-white bg-secondary p-5"
+              : date.date === "&#10003;"
+              ? "flex justify-center items-center h-5 w-5 rounded-full text-white bg-main p-5"
+              : "flex justify-center items-center h-5 w-5 rounded-full text-main bg-white p-5"
+          }
+          dangerouslySetInnerHTML={{ __html: date.date }}
+        />
+        <h2 className={date.date === 5 ? "text-secondary" : "text-black"}>
+          {date.day}
+        </h2>
+      </div>
+    );
+  });
+}
+
 function StatsPage() {
+  const icon1 = "<";
+  const icon2 = ">";
+
   return (
     <div className="container w-full">
+      <div className="flex justify-between items-center flex-row my-5 p-5">
+        <div className="text-main border-solid border-main rounded-lg py-0.5 px-2 border text-xs">
+          {icon1}
+        </div>
+        <h2 className="text-black font-medium text-2xl">September</h2>
+        <div className="text-main border-solid border-main rounded-lg py-0.5 px-2 border text-xs">
+          {icon2}
+        </div>
+      </div>
+      <div className="flex flex-row justify-center items-center gap-3 px-5">
+        <Date />
+      </div>
       <NavigationTab title="Skills" icon="..." />
       <SubjectProgress />
       <MenuTab />
