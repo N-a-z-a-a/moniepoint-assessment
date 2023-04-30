e475f9a24c30b1a26afa80683424e8c18b0c447c
N-a-z-a-a <jedwin.ezeh@gmail.com>
2023-04-30 20:31:07 +0100

    change the icon for the subject progress page

--- tech-assessment/src/components/SubjectProgress.jsx
+++ tech-assessment/src/components/SubjectProgress.jsx
@@ -1,43 +1,50 @@
+import {
+  BookOpenIcon,
+  PencilSquareIcon,
+  SpeakerWaveIcon,
+  MicrophoneIcon,
+} from "@heroicons/react/24/outline";
+
 function SubjectProgress() {
   const subjects = [
     {
       id: 1,
-      icon: "icon",
+      icon: <BookOpenIcon className="h-6 w-6" />,
       name: "Reading",
       progress: 100,
       status: "Completed",
     },
     {
       id: 1,
-      icon: "icon",
-      name: "Reading",
-      progress: 80,
+      icon: <PencilSquareIcon className="h-6 w-6" />,
+      name: "Writing",
+      progress: 85,
       status: "Good work",
     },
     {
       id: 1,
-      icon: "icon",
-      name: "Reading",
+      icon: <SpeakerWaveIcon className="h-6 w-6" />,
+      name: "Speaking",
       progress: 100,
       status: "Completed",
     },
     {
       id: 1,
-      icon: "icon",
-      name: "Reading",
+      icon: <MicrophoneIcon className="h-6 w-6" />,
+      name: "Listening",
       progress: 75,
       status: "Good work",
     },
     {
       id: 1,
-      icon: "icon",
-      name: "Reading",
-      progress: 75,
+      icon: <MicrophoneIcon className="h-6 w-6" />,
+      name: "Listening",
+      progress: 70,
       status: "Good work",
     },
     {
       id: 1,
-      icon: "icon",
+      icon: <BookOpenIcon className="h-6 w-6" />,
       name: "Reading",
       progress: 100,
       status: "Completed",
@@ -49,7 +56,7 @@ function SubjectProgress() {
         return (
           <div className="bg-white flex flex-col justify-center items-center py-4 gap-3 rounded-2xl">
             <div className="rounded-full h-14 w-14 bg-main text-white flex justify-center items-center">
-              {subject.icon}
+              <div>{subject.icon}</div>
             </div>
             <h2 className="text-black font-medium">{subject.name}</h2>
             <h2 className="text-gray-400 text-base font-light">
