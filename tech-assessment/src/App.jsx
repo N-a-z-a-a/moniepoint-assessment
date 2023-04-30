1e029d41a98a21cf3ec2ee6d831a460e83bd399c
N-a-z-a-a <jedwin.ezeh@gmail.com>
2023-04-30 17:13:44 +0100

    route the app

--- tech-assessment/src/App.jsx
+++ tech-assessment/src/App.jsx
@@ -1,11 +1,11 @@
 // import HomePage from "./pages/Homepage";
 import "./index.css";
-import StatsPage from "./pages/StatsPage";
-// import HomePage from "./pages/Homepage";
+// import StatsPage from "./pages/StatsPage";
+import HomePage from "./pages/Homepage";
 
 function App() {
-  // return <HomePage />;
-  return <StatsPage />;
+  return <HomePage />;
+  // return <StatsPage />;
 }
 
 export default App;
--- tech-assessment/src/components/MenuTab.jsx
+++ tech-assessment/src/components/MenuTab.jsx
@@ -1,10 +1,20 @@
+import { Link } from "react-router-dom";
+
 function MenuTab() {
   return (
     <div className="flex flex-row justify-around items-center bg-white px-5 py-7 rounded-t-3xl w-full fixed bottom-0 menu-tab">
-      <h2 className="text-gray-500">HO</h2>
-      <h2 className="text-gray-500">ST</h2>
-      <h2 className="text-gray-500">PE</h2>
-      <h2 className="text-gray-500">SE</h2>
+      <Link to={`/`} className="text-gray-500">
+        HO
+      </Link>
+      <Link to={`/stats`} className="text-gray-500">
+        ST
+      </Link>
+      <Link to={`/`} className="text-gray-500">
+        PE
+      </Link>
+      <Link to={`/`} className="text-gray-500">
+        SE
+      </Link>
     </div>
   );
 }
